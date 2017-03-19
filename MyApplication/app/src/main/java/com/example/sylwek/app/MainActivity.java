5b1d22fae54fd4ddaeb57f78653fbc3155cd929a
diaank <diaank@gmail.com>
2017-03-19 20:30:11 +0100

    init

--- MyApplication/app/src/main/java/com/example/sylwek/app/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sylwek/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sylwek.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "bleble",Toast.LENGTH_LONG);
     }
 }
