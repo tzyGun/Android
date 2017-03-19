20ea4870eb6811c7183e2ec545826234f0fb3837
tzyGun <sylwester.denko@gmail.com>
2017-03-19 22:22:04 +0100

    App2

---
+++ MyApplication/app/src/main/java/com/example/sylwek/app/Addition.java
@@ -0,0 +1,13 @@
+package com.example.sylwek.app;
+
+/**
+ * Created by Sylwek on 19.03.2017.
+ */
+
+
+public class Addition  implements Operation{
+    @Override
+    public float calculate(float a, float b) {
+        return a+b;
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/sylwek/app/Multiplication.java
@@ -0,0 +1,12 @@
+package com.example.sylwek.app;
+
+/**
+ * Created by Sylwek on 19.03.2017.
+ */
+
+public class Multiplication implements Operation {
+    @Override
+    public float calculate(float a, float b) {
+        return a*b;
+    }
+}
