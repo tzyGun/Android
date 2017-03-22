f07b90cf77913b559179501f2456f28dfb7df4b4
tzyGun <sylwester.denko@gmail.com>
2017-03-22 23:31:31 +0100

    app6

--- MyApplication/app/src/main/java/com/example/sylwek/app/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sylwek/app/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.sylwek.app;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    public final static String PASSING_VALUE= "PASSING_VALUE";
+    public final static String RETURNED_VALUE= "RETURNED_VALUE";
+    public final static int REQUEST_CODE= 1;
+    EditText first;
+    EditText second;
+    TextView addidtion;
+    TextView multiplication;
+    TextView subtraction;
+    TextView division;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        first = (EditText) findViewById(R.id.first_input);
+        second = (EditText) findViewById(R.id.second_input);
+
+
+        addidtion = (TextView) findViewById(R.id.addidtion);
+        multiplication = (TextView) findViewById(R.id.multiplication);
+        division = (TextView) findViewById(R.id.division);
+        subtraction = (TextView) findViewById(R.id.subtraction);
+
+
+
+
+    }
+    public void onClick(View view) {
+
+        String stringToPass= first.getText()
+                .toString()
+                .concat(",")
+                .concat(second.getText().toString());
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(PASSING_VALUE,stringToPass);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+
+            if (data.hasExtra(RETURNED_VALUE)) {
+                String result = data.getExtras().getString(RETURNED_VALUE).toString();
+
+                String returnedValues [] = result.split(",");
+                if (result != null && result.length() > 0) {
+                    Toast.makeText(getApplicationContext(), "Results calculated in previous activity", Toast.LENGTH_LONG).show();
+                }
+
+                addidtion.setText("Addidtion: "+ returnedValues[0]);
+                subtraction.setText("Subtraction: "+ returnedValues[1]);
+                multiplication.setText("Multiplication: "+ returnedValues[2]);
+                division.setText("Division: "+ returnedValues[3]);
+
+
+
+
+            }
+
+        }
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/sylwek/app/ResultActivity.java
@@ -0,0 +1,82 @@
+package com.example.sylwek.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.renderscript.Float2;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ResultActivity extends Activity {
+
+    TextView first;
+    TextView second;
+
+    String toPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        this.toPass=null;
+
+        first = (TextView) findViewById(R.id.first_var);
+        second = (TextView) findViewById(R.id.second_var);
+
+
+
+        Bundle extraMessage = getIntent().getExtras();
+        String message = extraMessage.getString(MainActivity.PASSING_VALUE);
+
+        String values [] = message.split(",");
+
+        float a = Float.parseFloat(values[0]);
+        float b = Float.parseFloat(values[1]);
+        first.setText(Float.toString(a));
+        second.setText(Float.toString(b));
+        this.toPass=calculate(a,b);
+
+
+
+        Toast.makeText(this,message , Toast.LENGTH_SHORT).show();
+    }
+
+    public void  finish(){
+        Intent intent = new Intent();
+
+        intent.putExtra(MainActivity.RETURNED_VALUE, this.toPass );
+
+        setResult(RESULT_OK,intent);
+        super.finish();
+
+    }
+
+    private String calculate(float a , float b) {
+        String result = null;
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    result = Float.toString(a + b).concat(",");
+                    break;
+                case 1:
+                    result += Float.toString(a - b).concat(",");
+                    break;
+                case 2:
+                    result += Float.toString(a * b).concat(",");
+                    break;
+                case 3:
+                    if (b != 0) {
+                        result += Float.toString(a / b);
+                    } else {
+                        result+= result.concat("0");
+                    }
+                    break;
+
+            }
+
+        }
+        return result;
+    }
+
+}
